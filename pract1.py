de2d39a221acf481359a190fc86ac46862e77d7c
Abhilashg11 <alexrog1122@gmail.com>
2023-05-31 13:58:36 +0530

    practice

--- pract1.py
+++ pract1.py
@@ -1,1 +1,13 @@
-print("hi how are you")
+# print("hi how are you")
+my_list=["bad","man","hi"]
+for i in my_list:
+    print(i)
+
+print(len(my_list))
+my_list.append("my")
+my_list.sort()
+print(my_list)
+
+mt_list1=[0] * 5
+print(mt_list1)
+print(my_list + mt_list1)
