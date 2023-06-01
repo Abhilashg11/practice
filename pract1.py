2f531d7dfc97a062887af625b011454b0f4f44d2
Abhilashg11 <alexrog1122@gmail.com>
2023-06-01 16:42:55 +0530

    tuples

--- pract1.py
+++ pract1.py
@@ -1,13 +1,21 @@
 # print("hi how are you")
-my_list=["bad","man","hi"]
-for i in my_list:
-    print(i)
+# my_list=["bad","man","hi"]
+# for i in my_list:
+#     print(i)
 
-print(len(my_list))
-my_list.append("my")
-my_list.sort()
-print(my_list)
+# print(len(my_list))
+# my_list.append("my")
+# my_list.sort()
+# print(my_list)
 
-mt_list1=[0] * 5
-print(mt_list1)
-print(my_list + mt_list1)
+# mt_list1=[0] * 5
+# print(mt_list1)
+# new_=my_list + mt_list1
+# print(new_[1:3])
+
+##tuples
+mytuple = tuple(["hi","mt","fir"])
+print(mytuple)
+
+print(mytuple[0])
+
