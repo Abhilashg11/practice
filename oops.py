4c91e481642f3b71e61c2bb246da56204195356f
Abhilashg11 <alexrog1122@gmail.com>
2023-06-17 23:54:37 +0530

    basic understanding of classes and objects

--- oops.py
+++ oops.py
@@ -1,14 +1,23 @@
 class item:
-    pass
+    def calculate(self,x,y):
+        return x * y
+
 
 item1 = item()
 item1.name = "phone"
 item1.price = 100
 item1.quantity = 5
+print(item1.calculate(item1.price,item1.quantity))
 
-print("hi")
+item2 = item()
+item2.name = "laptop"
+item2.price = 1000
+item2.quantity = 8
+print(item2.calculate(item2.price,item2.quantity))
 
-print(type(item1))
-print(type(item1.name))
-print(type(item1.price))
-print(type(item1.quantity))
+# print("hi")
+
+# print(type(item1))
+# print(type(item1.name))
+# print(type(item1.price))
+# print(type(item1.quantity))
