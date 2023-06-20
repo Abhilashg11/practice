1072df8051f026b08998ef97f65eea00c6baaadf
Abhilashg11 <alexrog1122@gmail.com>
2023-06-20 15:26:15 +0530

    update

--- discount.py
+++ discount.py
@@ -1,3 +1,4 @@
+import csv
 class item:
     pay_rate = 0.8
     all = []
@@ -14,13 +15,23 @@ class item:
         return self.price * self.quantity
     def apply_discount(self):
         self.price = self.price * item.pay_rate
+    @classmethod
+    def instantiate_from_csv(cls): 
+        with open('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items1 =  list(reader)
+        for i in  items1:
+            item(
+                name = i.get("name"),
+                price = float(i.get('price')),
+                quantity = int(i.get('quantity'))
+            )
+            
+
     def __repr__(self):
         return f"item('{self.name}',{self.price},{self.quantity})"  
-    def instantiate_from_csv(self): 
-        pass 
+    
 
 
+item.instantiate_from_csv()
 print(item.all)
-
-for i in item.all:
-    print(i.name)
