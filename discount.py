15ac4f5814aceab0c8a5d3cff2faf25f386b66a7
Abhilashg11 <alexrog1122@gmail.com>
2023-06-21 12:11:00 +0530

    update

--- discount.py
+++ discount.py
@@ -11,6 +11,7 @@ class item:
         self.quantity = quantity
 
         item.all.append(self)
+   
     def calculate(self):
         return self.price * self.quantity
     def apply_discount(self):
@@ -41,4 +42,22 @@ class item:
     
 
 
-print(item.is_integer(7.0))
+class phone(item):
+     all = []
+     def __init__(self,name:str,price:float,quantity=0,broken_phones=0):
+        assert price >= 0, f"price {price} is not greater then zero"
+        assert quantity >=0,f"quantity {quantity} is not greater than zero"
+        assert broken_phones >=0,f"quantity {broken_phones} is not greater than zero"
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.broken_phones = broken_phones
+
+        phone.all.append(self)
+
+phone1 = phone("jcphone",500,5,1)
+print(phone1.calculate())
+phone2 = phone("jscphone20",700,6)
+
+
+
